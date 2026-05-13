d367c20e01f5a54f6cca7d83c2204f34ab3fc3e0
sohan24-dev <sohan.explorer@gmail.com>
2026-05-13 23:18:44 +0600

    add

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,157 @@
+"use client"
+
+import {
+    Button,
+    Description,
+    FieldError,
+    FieldGroup,
+    Fieldset,
+    Form,
+    Input,
+    Label,
+    TextArea,
+    TextField,
+} from "@heroui/react";
+
+
+import Link from "next/link";
+import { GrGoogle } from "react-icons/gr";
+import { toast } from "react-toastify";
+import { useEffect, useRef } from "react";
+import { redirect, useSearchParams } from "next/navigation";
+import { authClient } from "@/app/lib/auth-client";
+
+const LoginForm = () => {
+
+    const searchParams = useSearchParams();
+    const error = searchParams.get("error");
+
+    const toastShown = useRef(false);
+
+    useEffect(() => {
+        if (error === "login_required" && !toastShown.current) {
+            toast.error("Please login first");
+            toastShown.current = true;
+        }
+    }, [error]);
+    const handleGoogleSignIn = async () => {
+        const data = await authClient.signIn.social({
+            provider: "google",
+        });
+
+        // console.log(data);
+    };
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const userdata = Object.fromEntries(formData);
+
+        // console.log(userdata);
+
+        const { data, error } = await authClient.signIn.email({
+            email: userdata.email,
+            password: userdata.password,
+            name: userdata.name,
+        });
+        if (data) {
+            toast.success("login success"
+            );
+            redirect('/')
+        }
+        // console.log("data:", data);
+        // console.log("error:", error);
+    };
+    return (
+        <Form
+            className="w-full max-w-md mx-auto bg-white shadow-2xl rounded-3xl p-8 border border-gray-100"
+            onSubmit={onSubmit}
+        >
+            <Fieldset>
+                <div className="text-center mb-6">
+                    <Fieldset.Legend className="text-3xl font-bold text-gray-800">
+                        Login Now
+                    </Fieldset.Legend>
+                </div>
+
+                <FieldGroup className="space-y-5">
+
+                    <TextField isRequired name="email" type="email">
+                        <Label className="text-sm font-medium text-gray-700">
+                            Email
+                        </Label>
+
+                        <Input
+                            placeholder="Enter your email"
+                            className="mt-2"
+                        />
+
+                        <FieldError />
+                    </TextField>
+
+                    <TextField
+                        isRequired
+                        name="password"
+                        type="password"
+                        minLength={8}
+                    >
+                        <Label>Password</Label>
+
+                        <Input placeholder="Enter your password" />
+
+                        <Description>
+                            Password must be at least 8 characters
+                        </Description>
+
+                        <FieldError />
+                    </TextField>
+                </FieldGroup>
+
+                <Fieldset.Actions className="flex flex-col sm:flex-row gap-3 mt-8">
+                    <Button
+                        type="submit"
+                        className="w-full bg-black text-white font-semibold py-3 rounded-xl hover:scale-[1.02] transition"
+                    >
+                        login
+                    </Button>
+
+                    <Button
+                        type="reset"
+                        variant="secondary"
+                        className="w-full py-3 rounded-xl"
+                    >
+                        Cancel
+                    </Button>
+                </Fieldset.Actions>
+                <div className="flex items-center">
+                    <div className="flex-1 h-px bg-gray-200" />
+                    <span className="px-3 text-xs text-gray-400">OR</span>
+                    <div className="flex-1 h-px bg-gray-200" />
+                </div>
+
+                {/* Google Login */}
+                <Button
+                    type="button"
+                    className="w-full flex items-center justify-center gap-2 border border-gray-100 py-3 rounded-xl hover:bg-gray-500 transition"
+                    onClick={handleGoogleSignIn}
+                >
+                    <GrGoogle />
+                    Continue with Google
+                </Button>
+
+                {/* Login Link */}
+                <p className="text-center text-sm text-gray-500 mt-2">
+                    Go register ?{" "}
+                    <Link
+                        href="/register"
+                        className="text-black font-semibold hover:underline"
+                    >
+                        Register
+                    </Link>
+                </p>
+            </Fieldset>
+        </Form>
+    );
+};
+
+export default LoginForm;
